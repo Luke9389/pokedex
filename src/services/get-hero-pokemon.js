515b869b9fd90d4d7317f13537545660c4cceffa
Luke Donahue <luke9389@gmail.com>
2019-08-18 17:40:05 -0700

    added page component

--- src/services/get-hero-pokemon.js
+++ src/services/get-hero-pokemon.js
@@ -1,6 +1,11 @@
-const url = 'https://alchemy-pokedex.herokuapp.com/api/pokedex?sort=_id';
+const apiUrl = 'https://alchemy-pokedex.herokuapp.com/api/pokedex?sort=id';
 
-export function getHeroPokemon() {
+export function getHeroPokemon(settings) {
+
+    const page = settings.page || 1; 
+
+    const url = `${apiUrl}&page=${page}`;
+    
     return fetch(url)
         .then(response => response.json());
 
